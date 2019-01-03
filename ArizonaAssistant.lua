25ccbeefe0dcb4b3db7291b2bb0563785d6a6505
IgorGarasimchyk <sunsobaki123331@gmail.com>
2019-01-03 22:56:54 +0200

    Update ArizonaAssistant.lua

--- ArizonaAssistant.lua
+++ ArizonaAssistant.lua
@@ -17,6 +17,9 @@ u8 = encoding.UTF8
 local auth_status = false
 -- ===================== [ Vars ] ====================== 
 
+local starup_window = imgui.ImBool(false)
+
+
 function sampev.onSendSpawn()
 	if auth_status == false then
 		sampAddChatMessage('[Информация] {7B68EE}Для авторизации используйте {FF0000}/auth', 0xDAA520)
@@ -30,8 +33,12 @@ function main()
   -- Загрузка сампа
   while true do
   	wait(10)
-  	-- Постоянные цикл
-  	
+  	if auth_status then
+    	if isKeyJustPressed(VK_F2) then -- Главное меню
+        imgui.Process = true
+        starup_window.v = not starup_window.v
+      end
+    end
   end
 end
 
@@ -40,10 +47,86 @@ function cmd_auth()
 		sampAddChatMessage('[Информация] {7B68EE}Вы уже авторизованы!', 0xDAA520)
 	else
     print('Starting load...')
+    load_startup_images()
+    sampAddChatMessage('[Информация] {7B68EE}Скрипт успешно загружен!', 0xDAA520)
 		auth_status = true
 	end
 end
 
-function imgui.OnDrawFrame()
-
+function load_startup_images()
+  arizonaLogo =  imgui.CreateTextureFromFile(getWorkingDirectory() .. '\\ArizonaAssistant\\images\\arizona-logo.png')
 end
+
+function imgui.OnDrawFrame()
+  if starup_window.v then
+    imgui.SetNextWindowSize(imgui.ImVec2(333, 333), imgui.Cond.FirstUseEver) -- меняем размер =============
+    imgui.SetNextWindowPos(imgui.ImVec2(333, 333))
+    imgui.Begin(u8'Главное меню', use_window_state, imgui.WindowFlags.NoResize + imgui.WindowFlags.NoMove + imgui.WindowFlags.NoCollapse)
+    imgui.Image(arizonaLogo, imgui.ImVec2(250, 200))
+    imgui.End()
+  end
+end
+
+-- ===================== [ Custom style ] ======================
+function apply_custom_style()
+  imgui.SwitchContext()
+  local style = imgui.GetStyle()
+  local colors = style.Colors
+  local clr = imgui.Col
+  local ImVec4 = imgui.ImVec4
+  local ImVec2 = imgui.ImVec2
+
+  style.WindowPadding = ImVec2(15, 15)
+  style.WindowRounding = 5.0
+  style.FramePadding = ImVec2(5, 5)
+  style.FrameRounding = 4.0
+  style.ItemSpacing = ImVec2(12, 8)
+  style.ItemInnerSpacing = ImVec2(8, 6)
+  style.IndentSpacing = 25.0
+  style.ScrollbarSize = 15.0
+  style.ScrollbarRounding = 9.0
+  style.GrabMinSize = 5.0
+  style.GrabRounding = 3.0
+
+  colors[clr.Text] = ImVec4(0.80, 0.80, 0.83, 1.00)
+  colors[clr.TextDisabled] = ImVec4(0.24, 0.23, 0.29, 1.00)
+  colors[clr.WindowBg] = ImVec4(0.06, 0.05, 0.07, 1.00)
+  colors[clr.ChildWindowBg] = ImVec4(0.07, 0.07, 0.09, 1.00)
+  colors[clr.PopupBg] = ImVec4(0.07, 0.07, 0.09, 1.00)
+  colors[clr.Border] = ImVec4(0.80, 0.80, 0.83, 0.88)
+  colors[clr.BorderShadow] = ImVec4(0.92, 0.91, 0.88, 0.00)
+  colors[clr.FrameBg] = ImVec4(0.10, 0.09, 0.12, 1.00)
+  colors[clr.FrameBgHovered] = ImVec4(0.24, 0.23, 0.29, 1.00)
+  colors[clr.FrameBgActive] = ImVec4(0.56, 0.56, 0.58, 1.00)
+  colors[clr.TitleBg] = ImVec4(0.10, 0.09, 0.12, 1.00)
+  colors[clr.TitleBgCollapsed] = ImVec4(1.00, 0.98, 0.95, 0.75)
+  colors[clr.TitleBgActive] = ImVec4(0.07, 0.07, 0.09, 1.00)
+  colors[clr.MenuBarBg] = ImVec4(0.10, 0.09, 0.12, 1.00)
+  colors[clr.ScrollbarBg] = ImVec4(0.10, 0.09, 0.12, 1.00)
+  colors[clr.ScrollbarGrab] = ImVec4(0.80, 0.80, 0.83, 0.31)
+  colors[clr.ScrollbarGrabHovered] = ImVec4(0.56, 0.56, 0.58, 1.00)
+  colors[clr.ScrollbarGrabActive] = ImVec4(0.06, 0.05, 0.07, 1.00)
+  colors[clr.ComboBg] = ImVec4(0.19, 0.18, 0.21, 1.00)
+  colors[clr.CheckMark] = ImVec4(0.80, 0.80, 0.83, 0.31)
+  colors[clr.SliderGrab] = ImVec4(0.80, 0.80, 0.83, 0.31)
+  colors[clr.SliderGrabActive] = ImVec4(0.06, 0.05, 0.07, 1.00)
+  colors[clr.Button] = ImVec4(0.10, 0.09, 0.12, 1.00)
+  colors[clr.ButtonHovered] = ImVec4(0.24, 0.23, 0.29, 1.00)
+  colors[clr.ButtonActive] = ImVec4(0.56, 0.56, 0.58, 1.00)
+  colors[clr.Header] = ImVec4(0.10, 0.09, 0.12, 1.00)
+  colors[clr.HeaderHovered] = ImVec4(0.56, 0.56, 0.58, 1.00)
+  colors[clr.HeaderActive] = ImVec4(0.06, 0.05, 0.07, 1.00)
+  colors[clr.ResizeGrip] = ImVec4(0.00, 0.00, 0.00, 0.00)
+  colors[clr.ResizeGripHovered] = ImVec4(0.56, 0.56, 0.58, 1.00)
+  colors[clr.ResizeGripActive] = ImVec4(0.06, 0.05, 0.07, 1.00)
+  colors[clr.CloseButton] = ImVec4(0.40, 0.39, 0.38, 0.16)
+  colors[clr.CloseButtonHovered] = ImVec4(0.40, 0.39, 0.38, 0.39)
+  colors[clr.CloseButtonActive] = ImVec4(0.40, 0.39, 0.38, 1.00)
+  colors[clr.PlotLines] = ImVec4(0.40, 0.39, 0.38, 0.63)
+  colors[clr.PlotLinesHovered] = ImVec4(0.25, 1.00, 0.00, 1.00)
+  colors[clr.PlotHistogram] = ImVec4(0.40, 0.39, 0.38, 0.63)
+  colors[clr.PlotHistogramHovered] = ImVec4(0.25, 1.00, 0.00, 1.00)
+  colors[clr.TextSelectedBg] = ImVec4(0.25, 1.00, 0.00, 0.43)
+  colors[clr.ModalWindowDarkening] = ImVec4(1.00, 0.98, 0.95, 0.73)
+end
+apply_custom_style()
