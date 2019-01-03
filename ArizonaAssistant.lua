4ddd08d66f308ead0e0dc35a81578572dc245041
IgorGarasimchyk <sunsobaki123331@gmail.com>
2019-01-03 22:14:12 +0200

    Update ArizonaAssistant.lua

--- ArizonaAssistant.lua
+++ ArizonaAssistant.lua
@@ -3,7 +3,6 @@ script_name('ArizonaAssistant')
 script_author('Jack Grimes and Lucas Delius')
 script_description('GlobalAssistant | Arizona Scottdale')
 script_properties("work-in-pause")
-
 -- ===================== [ Libs ] ======================
 require "lib.moonloader" 
 local keys = require "vkeys"
@@ -14,19 +13,27 @@ local sampev = require 'lib.samp.events'
 encoding.default = 'CP1251'
 u8 = encoding.UTF8
 
+local auth_status = false
+-- ===================== [ Vars ] ======================
+
+function sampev.onSendSpawn()
+	if auth_status == false then
+		sampAddChatMessage('[Информация] {7B68EE}Для авторизации используйте {FF0000}/auth', 0xDAA520)
+	end
+end
 
 function main()
   if not isSampLoaded() or not isSampfuncsLoaded() then return end
   while not isSampAvailable() do wait (100) end
   sampRegisterChatCommand('auth', cmd_auth)
-  -- Р—Р°РіСЂСѓР·РєР° СЃР°РјРїР°
+  -- Загрузка сампа
   while true do
   	wait(10)
-  	-- РџРѕСЃС‚РѕСЏРЅРЅС‹Рµ С†РёРєР»
+  	-- Постоянные цикл
   	
   end
 end
 
 function cmd_auth()
-	sampAddChatMessage("Auth start", -1)
+	auth_status = true
 end
